f3b03bc94f4132ea96e2400edc3c891f12145d1a
Brandon Wilson <brandon@coil.com>
2024-07-25 15:32:05 -0500

    add add worker

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,7 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { WorkerEntrypoint } from "cloudflare:workers";
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+export default class extends WorkerEntrypoint {
+  async fetch() { return new Response("Hello from Worker B"); }
+  
+  add(a: number, b: number) { return a + b; } 
+}
